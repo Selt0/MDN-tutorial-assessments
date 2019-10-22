cddf939bd43d45762216e37f8868318cceb3be91
Selt0 <chewedgum12@gmail.com>
2019-10-22 11:31:38 -0500

    Complete 'Looping Code'

--- JavaScript/Active Learning/buildingBlocks/Loops.js
+++ JavaScript/Active Learning/buildingBlocks/Loops.js
@@ -12,7 +12,7 @@ exit-condition - defines when the loop should stop
 final-expression - always evaluated (run) each time the loop has gone through. Usually used to increment, or in some cases decrement, the counter variable
 */
 
-const cats = ['Bill'.'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+const cats = ['Bill','Jeff', 'Pete', 'Biggles', 'Jasmin'];
 let info = 'My cats are called ';
 
 const para = document.querySelector('p');
@@ -88,7 +88,7 @@ btn.addEventListener('click', function() {
   input.value = '';
   input.focus();
   for (let i =0; i <contacts.length; i++) {
-    let splitContact.contacts[i].split(':');
+    let splitContact = contacts[i].split(':');
     if (splitContact[0].toLowerCase() === searchName) {
       para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
       break;
